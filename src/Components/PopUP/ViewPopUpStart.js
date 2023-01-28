5c390b7d89b2d1f4897733d5e821035e55e098cf
timdean27 <timdean27@yahoo.com>
2023-01-28 18:57:03 -0500

    added buttons to click through images on popup

--- src/Components/PopUP/ViewPopUpStart.js
+++ src/Components/PopUP/ViewPopUpStart.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 
 const ViewPopUpStart = ({ selectedMarker }) => {
+  const [iamgeIndex, setImageIndex] = useState(0)
+
+
+  const ImagehandleClick = (direction) => {
+    let currentImageIndex = iamgeIndex
+    if (direction === 'next') {
+      setImageIndex(currentImageIndex + 1);
+    } else if (direction === 'previous') {
+      setImageIndex(currentImageIndex - 1);
+    }
+
+  }
+
   return (
     <div>
       ViewPopUpStart
       <h3>{selectedMarker.name}</h3>
-      <img className="FirstIamge_POPup" src={selectedMarker.image} alt={`Img of ${selectedMarker.name}`}></img>
+      <div className="ImageContainer">
+      <img className="Iamge_POPup" src={selectedMarker.images[iamgeIndex]} alt="Gallery Image" />
+      <div>
+      {iamgeIndex > 0 ?<HiChevronLeft onClick={() => ImagehandleClick('previous')}></HiChevronLeft>:<HiChevronLeft className="placeholder"/>} 
+      {iamgeIndex < (selectedMarker.images.length -1) ?<HiChevronRight onClick={() => ImagehandleClick('next')}></HiChevronRight>:<HiChevronRight className="placeholder"/>}
+      </div>
+      </div>
     </div>
   );
 };
