ac85ada705caf64921b3d5de0409328bf52271da
timdean27 <timdean27@yahoo.com>
2023-01-31 14:34:24 -0500

    working on firebase auth and image uploading

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDVx0TkRV7_HB6w45RWOLM3saWdkWr-l4A",
+  authDomain: "you-map-dev-6723f.firebaseapp.com",
+  projectId: "you-map-dev-6723f",
+  storageBucket: "you-map-dev-6723f.appspot.com",
+  messagingSenderId: "473781569817",
+  appId: "1:473781569817:web:342491760e93b1fec69d88",
+  measurementId: "G-MB56FXRNQE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
